0b8fe6cc6cf84d8dc57016790490ea05bcfe89d5
jhyun-k <jhkim1238@naver.com>
2023-02-03 18:36:17 +0900

    today

--- src/todoIndex.js
+++ src/todoIndex.js
@@ -1,1 +1,5 @@
+const viewYear = date.getFullYear();
+const viewMonth = date.getMonth();
+const viewDate = date.getDate();
+
 document.querySelector('.year-month-date').textContent = `${viewYear}년 ${viewMonth + 1}월 ${viewDate}일`
